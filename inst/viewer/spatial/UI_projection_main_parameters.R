0dfd42f0c81946773f1d98b73dd6ebe4dd0eb566
duocang <wangxuesong29@gmail.com>
2026-09-16 11:04:43 +0200

    perf(viewer): share and safely clone CRB prototypes

--- inst/viewer/spatial/UI_projection_main_parameters.R
+++ inst/viewer/spatial/UI_projection_main_parameters.R
@@ -2,6 +2,7 @@
 ## UI elements to set main parameters for the projection.
 ##----------------------------------------------------------------------------##
 output[["spatial_projection_main_parameters_UI"]] <- renderUI({
+  req(identical(input[["sidebar"]], "spatial"))
   req(data_set())
   ## This output is evaluated even while the Spatial tab is hidden
   ## (suspendWhenHidden = FALSE below). For a data set without spatial data
@@ -118,6 +119,7 @@ output[["spatial_projection_main_parameters_UI"]] <- renderUI({
 })
 
 output[["spatial_projection_background_selector_UI"]] <- renderUI({
+  req(identical(input[["sidebar"]], "spatial"))
   req(data_set())
   req(length(availableSpatial()) > 0)
 
@@ -165,7 +167,10 @@ serverSideGeneSelector(
   session,
   "spatial_projection_feature_to_display",
   extra_triggers = function() input[["spatial_projection_plot_type"]],
-  active = function() length(availableSpatial()) > 0
+  active = function() {
+    identical(input[["sidebar"]], "spatial") &&
+      length(availableSpatial()) > 0
+  }
 )
 
 ## Co-expression channel gene pickers. Same server-side population + active gate
@@ -188,7 +193,10 @@ lapply(
       session,
       channel_id,
       extra_triggers = function() input[["spatial_projection_plot_type"]],
-      active = function() length(availableSpatial()) > 0
+      active = function() {
+        identical(input[["sidebar"]], "spatial") &&
+          length(availableSpatial()) > 0
+      }
     )
   }
 )
