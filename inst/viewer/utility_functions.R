bced90df267732776f485f9b19411615de21a0f2
duocang <wangxuesong29@gmail.com>
2026-09-16 11:03:09 +0200

    fix: key shared CRBs by absolute path

--- inst/viewer/utility_functions.R
+++ inst/viewer/utility_functions.R
@@ -2553,7 +2553,8 @@ get_or_load_crb <- function(
     configured_paths
   )
   cache_identity <- .runtimeBackendCacheIdentity(effective_backend)
-  cached <- .crb_process_cache[[path]]
+  cache_key <- normalizePath(path, winslash = "/", mustWork = FALSE)
+  cached <- .crb_process_cache[[cache_key]]
   if (!is.null(cached)) {
     if (!identical(cached$backend_identity, cache_identity)) {
       stop(
@@ -2574,7 +2575,7 @@ get_or_load_crb <- function(
   obj <- .attachExternalExpression(obj, path, effective_backend)
   obj <- .attachImmuneRepertoireBackend(obj)
   obj <- .attachSpatialMoleculeBackend(obj, path)
-  .crb_process_cache[[path]] <- list(
+  .crb_process_cache[[cache_key]] <- list(
     object = obj,
     backend_identity = cache_identity
   )
